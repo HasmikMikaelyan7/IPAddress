3401abc2a9fd8e402c50229ffc439a1e8e35ed53
Hasmik Mikaelyan <hasmikmikaelyan7@gmail.com>
2017-10-27 19:19:06 +0300

    Add project files.

---
+++ IPAddress_MVC/Controllers/HomeController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace IPAddress_MVC.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            string ipAddress = Request.ServerVariables["HTTP_X_FORWARDED_FOR"];
+            if (string.IsNullOrEmpty(ipAddress))
+            {
+                ipAddress = Request.ServerVariables["REMOTE_ADDR"];
+            }
+            ViewBag.IPAddress = ipAddress;
+
+            return View();
+        }
+    }
+}
---
+++ IPAddress_MVC/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="IPAddress_MVC.MvcApplication" Language="C#" %>
